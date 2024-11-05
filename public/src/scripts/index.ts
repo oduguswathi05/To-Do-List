f413330cc48456172ed946b160d49909bcf57fbe
Swathi <swathi.odugu@ascendion.com>
2024-11-05 17:48:09 +0530

    checkbox added

--- public/src/scripts/index.ts
+++ public/src/scripts/index.ts
@@ -1,29 +1,28 @@
 const inputElement = document.querySelector("#todo-input") as HTMLInputElement;
 const newTodoEl = document.querySelector("#new-todo") as HTMLElement;
 interface Todo {
-    title: string;
-    status: boolean;
+  title: string;
+  status: boolean;
 }
 
-let newTodo: Todo[] = []; 
+let newTodo: Todo[] = [];
 
-inputElement.addEventListener("keypress",(event:KeyboardEvent)=>{
-    if(event.key === "Enter"){
-        newTodo.push({title:inputElement.value,
-            status:false
-        })
-       renderNewTodo(newTodo)
-    }
+inputElement.addEventListener("keypress", (event: KeyboardEvent) => {
+  if (event.key === "Enter") {
+    newTodo.push({ title: inputElement.value, status: false });
+    renderNewTodo(newTodo);
+  }
 });
 
-function renderNewTodo(newTodoLi:Todo[]){
+function renderNewTodo(newTodoLi: Todo[]) {
+  let innerHTML = "";
+  newTodoLi.forEach((n) => {
+    innerHTML += `<div>
+                      <input type="checkbox" class="check">${n.title}
+                  </div>`;
+  });
 
-let innerHTML = "";
-newTodoLi.forEach((n)=>{
-    innerHTML += `<li>${n.title}</li>`
-});
- 
-newTodoEl.innerHTML = innerHTML
-  
- 
+  newTodoEl.innerHTML = innerHTML;
 }
+
+
